3d7087621ad4e0c3537dc7436621feddfd6282be
Ice Guo <maosilu.ice@gmail.com>
2017-11-02 17:43:22 +0800

    bug修改

--- php_connect1.php
+++ php_connect1.php
@@ -1,10 +1,11 @@
 <?php
 //通过参数形式连接数据库
 try{
-	$dsn = 'mysqli:host=localhost;dbname=test';
+	$dsn = 'mysql:host=localhost;dbname=test';
 	$username = 'root';
 	$passwd = '123456';
 	$dbh = new PDO($dsn, $username, $passwd);
+	var_dump($dbh);
 }catch(PDOException $e){
 	echo $e->getMessage();
 }
