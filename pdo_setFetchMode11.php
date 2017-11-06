d518a198abab56d3beefd5d867ed79bc0451acd3
Ice Guo <maosilu.ice@gmail.com>
2017-11-06 10:42:28 +0800

    三种设置输出数据形式的方式

--- pdo_setFetchMode11.php
+++ pdo_setFetchMode11.php
@@ -6,13 +6,16 @@ try{
 	$stmt = $dbh->prepare($sql);
 	$res = $stmt->execute();
 	echo "<pre>";
-	if($res){
-		while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
-			var_dump($row);
-			echo "<hr/>";
-		}
-	}
+	// if($res){
+	// 	while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+	// 		var_dump($row);
+	// 		echo "<hr/>";
+	// 	}
+	// }
 
+	// $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	// var_dump($rows);
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
 	$rows = $stmt->fetchAll();
 	var_dump($rows);
 }catch(PDOException $e){
