fa69e22c954aa8a40642772ae1f9d4519885ec1a
Ice Guo <maosilu.ice@gmail.com>
2017-11-06 17:23:53 +0800

    代码微调

--- doAction2.php
+++ doAction2.php
@@ -6,7 +6,7 @@ try{
 	$dbh = new PDO('mysql:host=localhost;dbname=test', 'root', '123456');
 	$sql = "SELECT * FROM pdo_user WHERE username=? AND password=?";
 	$stmt = $dbh->prepare($sql);
-	$stmt->execute(array(0=>$username, 1=>$password));
+	$stmt->execute(array($username, $password));
 	echo $stmt->rowCount();
 }catch(PDOException $e){
 	echo $e->getMessage();
