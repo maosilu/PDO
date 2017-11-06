8e6c99e7e2c81d91f8929dbd1f341d8608e406c3
Ice Guo <maosilu.ice@gmail.com>
2017-11-06 09:40:51 +0800

    pdo预处理和fetch的使用

---
+++ pdo_setFetchMode11.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-type:text/html;charset=utf-8');
+try{
+	$dbh = new PDO('mysql:host=localhost;dbname=test', 'root', '123456');
+	$sql = "SELECT * FROM pdo_user";
+	$stmt = $dbh->prepare($sql);
+	$res = $stmt->execute();
+	echo "<pre>";
+	if($res){
+		while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+			var_dump($row);
+			echo "<hr/>";
+		}
+	}
+
+	$rows = $stmt->fetchAll();
+	var_dump($rows);
+}catch(PDOException $e){
+	echo $e->getMessage();
+}
