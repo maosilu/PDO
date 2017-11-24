8a4f35dd6022e6ec31418bffae27d08484d665ae
Ice Guo <maosilu.ice@gmail.com>
2017-11-24 17:22:09 +0800

    注册功能：注册新用户，存入数据库

--- app/doAction.php
+++ app/doAction.php
@@ -16,9 +16,20 @@ $password = md5($_POST['password']);
 $emial = $_POST['emial'];
 //3.得到连接对象
 $PdoMysql = new PdoMysql();
-
+$table = 'user';
 if($act === 'reg'){
     //完成注册的功能
+    $regtime = time();
+    $token = md5($username,$password,$regtime);
+    $token_exptime = $regtime+24*3600; // 过期时间
+    $data = compact($username, $password, $email, $token, $token_exptime, $regtime);
+    $res = $PdoMysql->add($data, $table);
+    if($res){
+        
+    }else{
+        echo '用户注册失败，3秒钟后跳转到注册页面';
+        echo "<meta http-equiv='refresh' content='3;url=index.php#toregister'/>";
+    }
 }elseif($act === 'login'){
     //完成登录的功能
 }
